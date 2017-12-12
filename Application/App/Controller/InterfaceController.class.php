bd2f4d223a0980b49464d26a49abc24b7da14101
Liarman <287288189@qq.com>
2017-12-12 13:19:32 +0800

    modify bind controller

--- Application/App/Controller/InterfaceController.class.php
+++ Application/App/Controller/InterfaceController.class.php
@@ -127,7 +127,7 @@ class InterfaceController extends AppBaseController{
 
                     $data['bstatus']['code'] = 0;
                     $data['bstatus']['message'] = '获取成功';
-                    $data['camerasResult'] = $cameras;
+                    $data['data'] = $cameras;
                }else{
                     $data['bstatus']['code'] = C('APP_STATUS.STATUS_CODE_NOT_LOGIN');
                     $data['bstatus']['message'] = '登录失效，请重新登录';
